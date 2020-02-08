4f93406db56bd97b6f0112901b6c45aa908f9e3a
wolvecode <wolvecode@gmail.com>
2020-02-08 23:42:06 +0100

    changes

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,17 +3,17 @@ import './css/Login'
 
 export default function Login() {
   return (
-    <div className="row col-lg-6     offset-lg-3 border border-muted mt-5 mb-5">
-      <div className="form-group justify-content-center p-5">
+    <div className="col-lg-6">
+      <div className="form-style pt-5 pb-5">
         <h1 className="text-center">Login</h1>
-        <form action="#">
+        <form action="#" className="form-label">
           <div className="form pt-4">
             <input
               type="text"
               name="Email"
               id=""
               placeholder="Enter Email"
-              className="form-control"
+              className="form-control form-control-lg"
               required
             />
           </div>
@@ -23,12 +23,14 @@ export default function Login() {
               name="Password"
               id=""
               placeholder="Enter Password"
-              className="form-control"
+              className="form-control form-control-lg"
               required
             />
           </div>
           <div className="form pt-4">
-            <button className="form-control bg-info btn-group-lg">Login</button>
+            <button className="form-control bg-info btn btn-lg form-control-lg">
+              Login
+            </button>
           </div>
         </form>
       </div>
