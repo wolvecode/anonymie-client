a5fc49d4df9919a09d0738a7cbbcec875eabbdd0
wolvecode <wolvecode@gmail.com>
2020-02-09 22:10:48 +0100

    changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import './css/Home'
-import Login from './Login'
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="container-fluid pt-5 col-lg-8 offset-lg-2">
-        <div className="row border border-muted  justify-content-center">
-          <Login />
-        </div>
-      </div>
-    )
-  }
+export default function Home({ children }) {
+  return (
+    <div  className="container-fluid  border border-muted pt-5 mt-5 col-lg-8 offset-lg-2">
+      {children}
+    </div>
+  )
 }
-
-export default Home
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,39 +1,45 @@
 import React from 'react'
 import './css/Login'
+import Home from './Home'
 
 export default function Login() {
   return (
-    <div className="col-lg-6">
-      <div className="form-style pt-5 pb-5">
-        <h1 className="text-center">Login</h1>
-        <form action="#" className="form-label">
-          <div className="form pt-4">
-            <input
-              type="text"
-              name="Email"
-              id=""
-              placeholder="Enter Email"
-              className="form-control form-control-lg"
-              required
-            />
+    <Home>
+      <h1 className="display-4 font-weight-bolder m-4"> Anoymie</h1>
+      <div className="row justify-content-center">
+        <div className="col-lg-6">
+          <div className="form-style pb-5">
+            <h1 className="display-4 font-weight-bolder text-center">Login</h1>
+            <form action="#" className="form-label">
+              <div className="form pt-4">
+                <input
+                  type="text"
+                  name="Email"
+                  id=""
+                  placeholder="Enter Email"
+                  className="form-control form-control-lg"
+                  required
+                />
+              </div>
+              <div className="form pt-4">
+                <input
+                  type="password"
+                  name="Password"
+                  id=""
+                  placeholder="Enter Password"
+                  className="form-control form-control-lg"
+                  required
+                />
+              </div>
+              <div className="form pt-4">
+                <button className="form-control bg-info btn btn-lg form-control-lg">
+                  Login
+                </button>
+              </div>
+            </form>
           </div>
-          <div className="form pt-4">
-            <input
-              type="password"
-              name="Password"
-              id=""
-              placeholder="Enter Password"
-              className="form-control form-control-lg"
-              required
-            />
-          </div>
-          <div className="form pt-4">
-            <button className="form-control bg-info btn btn-lg form-control-lg">
-              Login
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
-    </div>
+    </Home>
   )
 }
