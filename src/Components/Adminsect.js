2fc3eaf63509a81cad45beca5db41e609ed54262
wolvecode <wolvecode@gmail.com>
2020-02-10 23:09:52 +0100

    suggestion list modify and add modal add

--- src/Components/Adminsect.js
+++ src/Components/Adminsect.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Home from './Home'
 import './css/AdminSect'
 
 export default function AdminSect() {
-  const [seacrh, setSearch] = useState('No suggestion available')
   return (
     <Home>
       <div className="row p-4">
@@ -18,25 +17,120 @@ export default function AdminSect() {
           />
         </div>
       </div>
-      <div className="row justify-content-center pb-5">
-        <div className="col-lg-8">
-          <ul className="list-group">
-            <li className="list-group-item border-danger text-center mb-2 font-weight-bold">
-              hel
-            </li>
-            <li className="list-group-item border-danger text-center mb-2 font-weight-bold">
-              hel
-            </li>
-            <li className="list-group-item border-danger text-center mb-2 font-weight-bold">
-              hel
-            </li>
-            <li className="list-group-item border-danger text-center mb-2 font-weight-bold">
-              hel
-            </li>
-            <li className="list-group-item border-danger text-center mb-2 font-weight-bold">
-              hel
-            </li>
-          </ul>
+      <div className="row pl-4">
+        <div className="col-lg-10">
+          <div>
+            <h3>Mr Bababtunde Approach</h3>
+          </div>
+          <div className="">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
+              placeat doloribus ipsa similique accusantium et veritatis odio est
+              quo voluptatum exercitationem, sit possimus repellendus aliquam
+              porro asperiores aperiam reiciendis maiores.
+            </p>
+          </div>
+          <div>
+            <div>
+              <h3>Mr Bababtunde Approach</h3>
+            </div>
+            <div className="">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
+                placeat doloribus ipsa similique accusantium et veritatis odio
+                est quo voluptatum exercitationem, sit possimus repellendus
+                aliquam porro asperiores aperiam reiciendis maiores.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div className="">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
+                placeat doloribus ipsa similique accusantium et veritatis odio
+                est quo voluptatum exercitationem, sit possimus repellendus
+                aliquam porro asperiores aperiam reiciendis maiores.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h3>Mr Bababtunde Approach</h3>
+            </div>
+            <div className="">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
+                placeat doloribus ipsa similique accusantium et veritatis odio
+                est quo voluptatum exercitationem, sit possimus repellendus
+                aliquam porro asperiores aperiam reiciendis maiores.
+              </p>
+            </div>
+          </div>
+          <div className="">
+            <div className="">
+              <h3 className="inline">Mr Bababtunde Approach</h3>
+            </div>
+            <div className="">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
+                placeat doloribus ipsa similique accusantium et veritatis odio
+                est quo voluptatum exercitationem, sit possimus repellendus
+                aliquam porro asperiores aperiam reiciendis maiores.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="ml-auto">
+          <button
+            type="button"
+            className="btn"
+            data-toggle="modal"
+            data-target="#exampleModalCenter"
+          >
+            <i className="add fab fa-medrt"></i>
+          </button>
+
+          <div
+            className="modal fade"
+            id="exampleModalCenter"
+            tabIndex="-1"
+            role="dialog"
+            aria-labelledby="exampleModalCenterTitle"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog modal-dialog-centered" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLongTitle">
+                    Modal title
+                  </h5>
+                  <button
+                    type="button"
+                    className="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">Hello world</div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button type="button" className="btn btn-primary">
+                    Save changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Home>
