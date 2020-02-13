52a4755b890bad9e0e3823f07e8cb188debb4735
wolvecode <wolvecode@gmail.com>
2020-02-13 21:58:19 +0100

    modify

--- src/Components/Adminsect.js
+++ src/Components/Adminsect.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Home from './Home'
+import Modal from './Modal'
 import './css/style.css'
 
 export default function AdminSect() {
   return (
     <Home>
-      <div className="row p-4">
+      <div className="row p-3">
         <div className="">
           <h1 className="display-4 font-weight-bolder">Anoymie</h1>
         </div>
@@ -19,11 +20,13 @@ export default function AdminSect() {
       </div>
       <div className="row pl-4 pt-5">
         <div className="">
-          <div className="row pl-3 pr-4">
-            <h5 className="">Mr Bababtunde Approach</h5>
-            <div className="btn-group dropup ml-auto">
+          <div className="row pl-3 pr-3">
+            <a href="#">
+              <h5 className="">Mr Bababtunde Approach</h5>
+            </a>
+            <div className="btn-group ml-auto">
               <button
-                className=" btn"
+                className="btn"
                 data-toggle="dropdown"
                 aria-haspopup="true"
                 aria-expanded="false"
@@ -31,8 +34,8 @@ export default function AdminSect() {
               >
                 <i class="ml-auto fas fa-ellipsis-v"></i>
               </button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">
+              <div class="dropdown-menu ">
+                <a class="dropdown-item bg-muted" href="#">
                   Delete
                 </a>
               </div>
@@ -47,11 +50,26 @@ export default function AdminSect() {
             </p>
           </div>
           <div>
-            <div className="row pl-3 pr-4">
-              <h5 className="">Mr Bababtunde Approach</h5>
-              <button className="ml-auto btn">
-                <i class="ml-auto fas fa-ellipsis-v"></i>
-              </button>
+            <div className="row pl-3 pr-3">
+              <a href="#">
+                <h5 className="">Mr Bababtunde Approach</h5>
+              </a>
+              <div className="btn-group ml-auto">
+                <button
+                  className=" btn"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  type="button"
+                >
+                  <i class="ml-auto fas fa-ellipsis-v"></i>
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">
+                    Delete
+                  </a>
+                </div>
+              </div>
             </div>
             <div className="">
               <p>
@@ -63,11 +81,26 @@ export default function AdminSect() {
             </div>
           </div>
           <div>
-            <div className="row pl-3 pr-4">
-              <h5 className="">Mr Bababtunde Approach</h5>
-              <button className="ml-auto btn">
-                <i class="ml-auto fas fa-ellipsis-v"></i>
-              </button>
+            <div className="row pl-3 pr-3">
+              <a href="#">
+                <h5 className="">Mr Bababtunde Approach</h5>
+              </a>
+              <div className="btn-group ml-auto">
+                <button
+                  className=" btn"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  type="button"
+                >
+                  <i class="ml-auto fas fa-ellipsis-v"></i>
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">
+                    Delete
+                  </a>
+                </div>
+              </div>
             </div>
             <div className="">
               <p>
@@ -79,11 +112,26 @@ export default function AdminSect() {
             </div>
           </div>
           <div>
-            <div className="row pl-3 pr-4">
-              <h5 className="">Mr Bababtunde Approach</h5>
-              <button className="ml-auto btn">
-                <i class="ml-auto fas fa-ellipsis-v"></i>
-              </button>
+            <div className="row pl-3 pr-3">
+              <a href="#">
+                <h5 className="">Mr Bababtunde Approach</h5>
+              </a>
+              <div className="btn-group ml-auto">
+                <button
+                  className=" btn"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  type="button"
+                >
+                  <i class="ml-auto fas fa-ellipsis-v"></i>
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">
+                    Delete
+                  </a>
+                </div>
+              </div>
             </div>
             <div className="">
               <p>
@@ -95,11 +143,88 @@ export default function AdminSect() {
             </div>
           </div>
           <div className="">
-            <div className="row pl-3 pr-4">
-              <h5 className="">Mr Bababtunde Approach</h5>
-              <button className="ml-auto btn">
-                <i class="ml-auto fas fa-ellipsis-v"></i>
-              </button>
+            <div className="row pl-3 pr-3">
+              <a href="#">
+                <h5 className="">Mr Bababtunde Approach</h5>
+              </a>
+              <div className="btn-group ml-auto">
+                <button
+                  className=" btn"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  type="button"
+                >
+                  <i class="ml-auto fas fa-ellipsis-v"></i>
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">
+                    Delete
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
+                placeat doloribus ipsa similique accusantium et veritatis odio
+                est quo voluptatum exercitationem, sit possimus repellendus
+                aliquam porro asperiores aperiam reiciendis maiores.
+              </p>
+            </div>
+          </div>
+          <div className="">
+            <div className="row pl-3 pr-3">
+              <a href="#">
+                <h5 className="">Mr Bababtunde Approach</h5>
+              </a>
+              <div className="btn-group ml-auto">
+                <button
+                  className=" btn"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  type="button"
+                >
+                  <i class="ml-auto fas fa-ellipsis-v"></i>
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">
+                    Delete
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
+                placeat doloribus ipsa similique accusantium et veritatis odio
+                est quo voluptatum exercitationem, sit possimus repellendus
+                aliquam porro asperiores aperiam reiciendis maiores.
+              </p>
+            </div>
+          </div>
+          <div className="">
+            <div className="row pl-3 pr-3">
+              <a href="#">
+                <h5 className="">Mr Bababtunde Approach</h5>
+              </a>
+              <div className="btn-group ml-auto">
+                <button
+                  className=" btn"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  type="button"
+                >
+                  <i class="ml-auto fas fa-ellipsis-v"></i>
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">
+                    Delete
+                  </a>
+                </div>
+              </div>
             </div>
             <div className="">
               <p>
@@ -113,83 +238,7 @@ export default function AdminSect() {
         </div>
       </div>
       <div className="row">
-        <div className="">
-          <button
-            className="btn fixed"
-            display="static"
-            type="button"
-            data-toggle="modal"
-            data-target="#exampleModalCenter"
-          >
-            <i className="fab fa-medrt"></i>
-          </button>
-        </div>
-        <div className="ml-auto">
-          <div
-            className="modal fade"
-            id="exampleModalCenter"
-            tabIndex="-1"
-            role="dialog"
-            aria-labelledby="exampleModalCenterTitle"
-            aria-hidden="true"
-          >
-            <div className="modal-dialog modal-dialog-centered" role="document">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title" id="exampleModalLongTitle">
-                    Add Suggestion
-                  </h5>
-                  <button
-                    type="button"
-                    className="close"
-                    data-dismiss="modal"
-                    aria-label="Close"
-                  >
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div className="modal-body">
-                  <form className="col-lg-10">
-                    <div className="form-group">
-                      <label for="recipient-name" className="form-label">
-                        Title
-                      </label>
-                      <input
-                        type="text"
-                        className="border-danger form-control"
-                        id="recipient-name"
-                      />
-                    </div>
-                    <div className="form-group">
-                      <label for="message-text" className="form-label">
-                        Description
-                      </label>
-                      <textarea
-                        className="border-danger form-control"
-                        id="message-text"
-                      ></textarea>
-                    </div>
-                    <button
-                      type="button"
-                      className="btn btn-group-lg b-style btn-primary"
-                    >
-                      Add
-                    </button>
-                  </form>
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-dismiss="modal"
-                  >
-                    Close
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Modal />
       </div>
     </Home>
   )
