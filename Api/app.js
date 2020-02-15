f162b6423aa672031845d1f4cce67abc71cc8ee6
wolvecode <wolvecode@gmail.com>
2020-02-15 04:56:19 +0100

    changes

--- Api/app.js
+++ Api/app.js
@@ -70,29 +70,6 @@ app.post('/suggestion', (req, res) => {
   next()
 })
 
-/*****
- COMMENT
- */
-app.get('/comment', (req, res, next) => {
-  res.send(comments)
-  next()
-})
-
-app.post('/comment', (req, res) => {
-  const { error } = val(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
-
-  const comment = {
-    id: comments.length + 1,
-    title: req.body.title,
-    description: req.body.description
-  }
-
-  comments.push(comment)
-  res.send(comment)
-})
-
-/*** */
 app.put('/suggestion/:id', (req, res) => {
   const suggestion = suggestions.find(c => c.id === parseInt(req.params.id))
 
@@ -124,6 +101,45 @@ app.delete('/suggestion/:id', (req, res) => {
 
   res.send(suggestion)
 })
+
+/*****
+ COMMENT
+ */
+app.get('/comment', (req, res, next) => {
+  res.send(comments)
+  next()
+})
+
+app.post('/comment', (req, res) => {
+  const { error } = val(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  const comment = {
+    id: comments.length + 1,
+    title: req.body.title,
+    description: req.body.description
+  }
+
+  comments.push(comment)
+  res.send(comment)
+})
+
+app.put('/comment/:id', (req, res) => {
+  const comment = comments.find(c => c.id === parseInt(req.params.id))
+  if (!comment) return res.status(404).send('No comment with the particular id')
+
+  const { error } = val(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  comt = {
+    title: req.body.title,
+    description: req.body.description
+  }
+
+  res.send(comt)
+})
+
+/*** */
 
 function val(suggestion) {
   const schema = {
