4535fc9fa1e0e12bd82e8d6bcf1dcad6e17b9f5b
wolvecode <wolvecode@gmail.com>
2020-02-26 21:17:46 +0100

    changes

--- Api/app.js
+++ Api/app.js
@@ -1,17 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const Joi = require('joi')
 const suggestion = require('./router/suggestion')
 const comment = require('./router/comment')
 app.use(express.json())
+app.use(express.urlencoded())
+
+mongoose
+  .connect('mongodb://localhost/anoymie', { useNewUrlParser: true })
+  .then(() => console.log('connected to database'))
+  .catch(err => console.err('inavlid connection'))
 
 app.use(suggestion)
-app.use(comment)
-/*****
- COMMENT
- */
-
-/*** */
+app.use('/', comment)
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
