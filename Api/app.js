21e812b9dc93d4e2046f2c2411797ddd9f007fda
wolvecode <wolvecode@gmail.com>
2020-02-26 21:26:36 +0100

    changes in router path

--- Api/app.js
+++ Api/app.js
@@ -12,8 +12,8 @@ mongoose
   .then(() => console.log('connected to database'))
   .catch(err => console.err('inavlid connection'))
 
-app.use(suggestion)
-app.use('/', comment)
+app.use('/suggestion', suggestion)
+app.use('/comment', comment)
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
