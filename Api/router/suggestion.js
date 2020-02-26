acba04bf4f126aff348c45d57e7453a4d211bb2f
wolvecode <wolvecode@gmail.com>
2020-02-26 21:18:22 +0100

    refacor using asyn and await

--- Api/router/suggestion.js
+++ Api/router/suggestion.js
@@ -1,57 +1,81 @@
 const express = require('express')
 const router = express.Router()
 const Joi = require('joi')
+const mongoose = require('mongoose')
 
-const suggestions = [
-  {
-    id: 1,
-    title: 'why mr babatunde',
-    description: 'you are open to talk about mr babatunde last class'
+// const suggestions = [
+//   {
+//     id: 1,
+//     title: 'why mr babatunde',
+//     description: 'you are open to talk about mr babatunde last class'
+//   },
+//   {
+//     id: 2,
+//     title: 'why mr babatunde',
+//     description: 'you are open to talk about mr babatunde last class'
+//   },
+//   {
+//     id: 3,
+//     title: 'why mr babatunde',
+//     description: 'you are open to talk about mr babatunde last class'
+//   }
+// ]
+
+const suggestionSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
   },
-  {
-    id: 2,
-    title: 'why mr babatunde',
-    description: 'you are open to talk about mr babatunde last class'
-  },
-  {
-    id: 3,
-    title: 'why mr babatunde',
-    description: 'you are open to talk about mr babatunde last class'
+  description: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255
   }
-]
-
-router.get('/suggestion', (req, res, next) => {
-  res.send(suggestions)
-  next()
 })
 
-router.get('/suggestion/:id', (req, res, next) => {
-  const suggestion = suggestions.find(c => c.id === parseInt(req.params.id))
-  if (!suggestion)
-    return res
-      .status(404)
-      .send('The suggestion with the given ID was not found.')
+const Suggestion = mongoose.model('Suggestion', suggestionSchema)
+
+router.get('/suggestion', async (req, res, next) => {
+  const suggestion = await Suggestion.find()
   res.send(suggestion)
   next()
 })
 
-router.post('/suggestion', (req, res) => {
+router.get('/suggestion/:id', async (req, res, next) => {
+  try {
+    const suggestion = await Suggestion.findById(req.params.id)
+    if (!suggestion)
+      return res
+        .status(404)
+        .send('The suggestion with the given ID was not found.')
+    res.send(suggestion)
+    next()
+  } catch (err) {
+    console.log(err.message)
+  }
+})
+
+router.post('/suggestion', async (req, res) => {
   const { error } = val(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
-  const suggestion = {
-    id: suggestions.length + 1,
+  let suggestion = new Suggestion({
     title: req.body.title,
     description: req.body.description
-  }
-
-  suggestions.push(suggestion)
+  })
+  suggestion = await suggestion.save()
   res.send(suggestion)
-  next()
 })
 
-router.put('/suggestion/:id', (req, res) => {
-  const suggestion = suggestions.find(c => c.id === parseInt(req.params.id))
+router.put('/suggestion/:id', async (req, res) => {
+  const suggestion = await Suggestion.findOneAndUpdate(
+    req.params.id,
+    { title: req.body.title, description: req.body.description },
+    { new: true }
+  )
 
   const { error } = val(req.body)
   if (error) return res.status(400).send(error.details[0].message)
@@ -61,39 +85,31 @@ router.put('/suggestion/:id', (req, res) => {
       .status(404)
       .send('The suggestion with the given ID was not found.')
 
-  suggest = {
-    title: req.body.title,
-    description: req.body.description
-  }
-
-  res.send(suggest)
+  res.send(suggestion)
 })
 
-router.delete('/suggestion/:id', (req, res) => {
-  const suggestion = suggestions.find(c => c.id === parseInt(req.params.id))
+router.delete('/suggestion/:id', async (req, res) => {
+  const suggestion = await Suggestion.findByIdAndRemove(req.params.id)
+
   if (!suggestion)
     return res
       .status(404)
       .send('The suggestion with the given ID was not found.')
-
-  const index = suggestions.indexOf(suggestion)
-  suggestions.splice(index, 1)
 
   res.send(suggestion)
 })
 
 function val(suggestion) {
-    const schema = {
-      title: Joi.string()
-        .min(3)
-        .required(),
-      description: Joi.string()
-        .min(3)
-        .required()
-    }
-  
-    return Joi.validate(suggestion, schema)
+  const schema = {
+    title: Joi.string()
+      .min(3)
+      .required(),
+    description: Joi.string()
+      .min(3)
+      .required()
   }
-  
+
+  return Joi.validate(suggestion, schema)
+}
 
 module.exports = router
