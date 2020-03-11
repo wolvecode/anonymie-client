cb8c18643622f481d8dc536e5e18658875537110
wolvecode <wolvecode@gmail.com>
2020-03-11 23:34:56 +0100

    changes

--- Api/router/admin.js
+++ Api/router/admin.js
@@ -1,14 +1,10 @@
 const express = require('express')
-const bcrypt = require('bcrypt')
+const LocalStrategy = require('passport-local').Strategy
 const auth = require('../middleware/auth')
-const mongoose = require('mongoose')
 const _ = require('lodash')
-const jwt = require('jsonwebtoken')
-const config = require('config')
 const router = express.Router()
 const { Admin, validate } = require('../model/admin')
 const { getAllAdmin, addAdmin } = require('../controllers/adminController')
-
 
 router.get('/me', auth, async (req, res) => {
   const admin = await Admin.findById(req.admin._id).select('-password')
