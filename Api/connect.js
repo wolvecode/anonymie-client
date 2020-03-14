48f8219167ad1dae913c802d1a0ec07533288ee4
wolvecode <wolvecode@gmail.com>
2020-03-14 21:14:27 +0100

    seprate client and servera(api) codebase

--- Api/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose
-  .connect('mongodb://localhost/anoymie', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('connected to database'))
-  .catch(err => console.err('inavlid connection'))
