952f8c29da089a61de76e2846d52c4cd95cb9b04
wolvecode <wolvecode@gmail.com>
2020-03-27 22:40:00 +0100

    add utils to send request to the api

---
+++ src/utils/request.js
@@ -0,0 +1,24 @@
+const baseUrl = 'http://127.0.0.1:5000'
+
+exports.signUp = (email, password) => {
+  const data = { email, password }
+  return fetch(baseUrl + 'signup', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+}
+
+exports.login = (email, password) => {
+  const data = { email, password }
+  return fetch(baseUrl + 'login', {
+    method: 'post',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+}
