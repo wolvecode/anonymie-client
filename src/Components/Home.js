b7359de05cb701602c3ebd80397efd9bcbeb46d4
wolvecode <wolvecode@gmail.com>
2020-04-21 23:14:02 +0100

    changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../style/style'
 
 export default function Home({ children }) {
   return (
@@ -10,30 +11,44 @@ export default function Home({ children }) {
             <p>nonymi</p>
           </div>
           <p className="sign-text">Comment Anonymously</p>
-          <form>
-            <div class="form-group">
-              <label for="exampleFormControlTextarea1">Example textarea</label>
+          <form className="text-center select-op">
+            <div className="justify-content-center">
+              <select class="form-control col-md-4 m-4">
+                <option>Lorem ipsum dolor sit amet.</option>
+                <option>Lorem ipsum dolor sit amet.</option>
+                <option>Lorem ipsum dolor sit amet.</option>
+                <option>Lorem ipsum dolor sit amet.</option>
+                <option>Lorem ipsum dolor sit amet</option>
+              </select>
+            </div>
+            <div class="form-group ">
               <textarea
-                class=""
+                className="m-auto display-block"
                 id="exampleFormControlTextarea1"
-                rows="3"
+                rows="10"
+                cols="50"
               ></textarea>
+            </div>
+            <div className="but text-center">
+              <button>Submit</button>
             </div>
           </form>
         </div>
-        <div className="col-lg-5 col-md-5 hidden-sm-down side2 p-0">
+        <div className="col-lg-5 col-md-5 hidden-sm-down side2 p-0 pt-5">
           <div className="grad1"></div>
           <div className="justify">
             <div className="grad2"></div>
             <h3>Hello!</h3>
-            <p>
+            <span className="">
               Welcome to Anonymi, a platform that enables <br /> random users to
-              comment base on suggestion <br /> suggestion that relate to them
-              (express their feelings)
-            </p>
+              comment base on suggestion <br />
+              hhjds hdhh
+              <br />
+              hhjds
+            </span>
           </div>
-          <div className="grad3"></div>
-          <div className="grad4"></div>
+          <div className="grad3 mt-5"></div>
+          <div className="grad4 mt-5"></div>
         </div>
       </div>
     </div>
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,10 +2,6 @@ import React from 'react'
 import '../style/style'
 
 class Sidebar extends React.Component {
-  constructor() {
-    super()
-  }
-
   render() {
     return (
       <div>
@@ -15,19 +11,19 @@ class Sidebar extends React.Component {
             class="sidebar-expanded d-none d-md-block"
           >
             <div>
-              <div className="logo-s justify-content-center">
-                <i className="far fa-user "></i>
-                <p className="i">nonymi</p>
+              <div className="logo-s justify-content-center pt-2">
+                <i className="far fa-user pt-2"></i>
+                <span className="bold">nonymi</span>
               </div>
               <div className="line justify-content-center"></div>
             </div>
 
-            <ul class="list-group">
+            <ul class="list-group pl-4">
               <a
                 href="#submenu1"
                 data-toggle="collapse"
                 aria-expanded="false"
-                class="b-color list-group-item list-group-item-action flex-column align-items-start"
+                class="b-st list-group-item align-items-start"
               >
                 <div class="d-flex w-100 justify-content-center align-items-center">
                   <span class="fa fa-dashboard fa-fw mr-3"></span>
@@ -39,7 +35,7 @@ class Sidebar extends React.Component {
               <div id="submenu1" class="collapse sidebar-submenu">
                 <a
                   href="#"
-                  class="list-group-item list-group-item-action b-color text-white"
+                  class="list-group-item list-group-item-action b-st text-white"
                 >
                   <span class="menu-collapsed">Chahgag</span>
                 </a>
@@ -48,7 +44,7 @@ class Sidebar extends React.Component {
                 href="#submenu2"
                 data-toggle="collapse"
                 aria-expanded="false"
-                class="b-color list-group-item list-group-item-action flex-column align-items-start"
+                class="b-st list-group-item list-group-item-action flex-column align-items-start"
               >
                 <div class="d-flex w-100 justify-content-center align-items-center">
                   <span class="fa fa-user fa-fw mr-3"></span>
@@ -57,30 +53,21 @@ class Sidebar extends React.Component {
                   <span class="submenu-icon ml-auto"></span>
                 </div>
               </a>
-              <a
-                href="#"
-                class="b-color list-group-item list-group-item-action"
-              >
+              <a href="#" class="b-st list-group-item list-group-item-action">
                 <div class="d-flex w-100 justify-content-center align-items-center">
                   <span class="fa fa-tasks fa-fw mr-3"></span>
                   <span class="menu-collapsed">Related Report</span>
                   <span class="submenu-icon ml-auto"></span>
                 </div>
               </a>
-              <a
-                href="#"
-                class="b-color list-group-item list-group-item-action"
-              >
+              <a href="#" class="b-st list-group-item list-group-item-action">
                 <div class="d-flex w-100 justify-content-center align-items-center">
                   <span class="fa fa-calendar fa-fw mr-3"></span>
                   <span class="menu-collapsed">Stared</span>
                   <span class="submenu-icon ml-auto"></span>
                 </div>
               </a>
-              <a
-                href="#"
-                class="b-color list-group-item list-group-item-action"
-              >
+              <a href="#" class="b-st list-group-item list-group-item-action">
                 <div class="d-flex w-100 justify-content-center align-items-center">
                   <span class="fa fa-envelope-o fa-fw mr-3"></span>
                   <span class="menu-collapsed">
@@ -91,39 +78,41 @@ class Sidebar extends React.Component {
                 </div>
               </a>
               <div className="menu-down">
-                <a
-                  href="#"
-                  class="b-color list-group-item list-group-item-action"
-                >
-                  <div class="d-flex w-100 justify-content-center align-items-center">
-                    <span class="fa fa-question fa-fw mr-3"></span>
-                    <span class="menu-collapsed">Tools</span>
-                    <span class="submenu-icon ml-auto"></span>
-                  </div>
-                </a>
-                <a
-                  href="#"
-                  class="b-color list-group-item list-group-item-action"
-                >
-                  <div class="d-flex w-100 justify-content-center align-items-center">
-                    <span class="fa fa-question fa-fw mr-3"></span>
-                    <span class="menu-collapsed">Report</span>
-                    <span class="submenu-icon ml-auto"></span>
-                  </div>
-                </a>
-                <a
-                  href="#top"
-                  data-toggle="sidebar-colapse"
-                  class="b-color list-group-item list-group-item-action d-flex align-items-center"
-                >
-                  <div class="d-flex w-100 justify-content-center align-items-center">
-                    <span id="collapse-icon" class="fa fa-2x mr-3"></span>
-                    <span id="collapse-text" class="menu-collapsed">
-                      Collapse
-                    </span>
-                    <span class="submenu-icon ml-auto"></span>
-                  </div>
-                </a>
+                <div className="move-down">
+                  <a
+                    href="#"
+                    class="b-st list-group-item list-group-item-action"
+                  >
+                    <div class="d-flex w-100 justify-content-center align-items-center">
+                      <span class="fa fa-question fa-fw mr-3"></span>
+                      <span class="menu-collapsed">Tools</span>
+                      <span class="submenu-icon ml-auto"></span>
+                    </div>
+                  </a>
+                  <a
+                    href="#"
+                    class="b-st list-group-item list-group-item-action"
+                  >
+                    <div class="d-flex w-100 justify-content-center align-items-center">
+                      <span class="fa fa-question fa-fw mr-3"></span>
+                      <span class="menu-collapsed">Report</span>
+                      <span class="submenu-icon ml-auto"></span>
+                    </div>
+                  </a>
+                  <a
+                    href="#top"
+                    data-toggle="sidebar-colapse"
+                    class="b-st list-group-item list-group-item-action d-flex align-items-center"
+                  >
+                    <div class="d-flex w-100 justify-content-center align-items-center">
+                      <span id="collapse-icon" class="fa fa-2x mr-2"></span>
+                      <span id="collapse-text" class="menu-collapsed">
+                        Collapse
+                      </span>
+                      <span class="submenu-icon ml-auto"></span>
+                    </div>
+                  </a>
+                </div>
               </div>
             </ul>
           </div>
