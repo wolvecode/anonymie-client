396e47a9bc9579dd23efa8bfd60c07e1b91b7bf2
wolvecode <wolvecode@gmail.com>
2020-04-30 18:21:54 +0100

    modify

--- src/Components/CommentStat.js
+++ src/Components/CommentStat.js
@@ -5,20 +5,23 @@ import { Chart } from './Chart'
 import { Stat } from './Stat'
 
 class CommentStat extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
   }
   render() {
     return (
       <Sidebar>
-        <div className="row p-0  pt-5">
-          <div className="col-md-5">
-            {' '}
-            <Stat />
-          </div>
-          <div className="col-md-7">
-            {' '}
-            <Chart />
+        <div className="col-md-10 offset-md-2">
+          {' '}
+          <div className="row p-0  pt-5">
+            <div className="col-md-5">
+              {' '}
+              <Stat />
+            </div>
+            <div className="col-md-7">
+              {' '}
+              <Chart />
+            </div>
           </div>
         </div>
       </Sidebar>
