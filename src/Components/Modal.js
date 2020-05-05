bf3c73e2f3c7fbaef1c07c22460ffd9a5b0cf2f0
wolvecode <wolvecode@gmail.com>
2020-05-05 23:50:32 +0100

    add modal tohandle add suggestion

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,82 +1,106 @@
 import React from 'react'
+import { navigate, redirectTo } from '@reach/router'
+import axios from 'axios'
+import '../style/style.css'
 
-export default function Modal() {
-  return (
-    <div>
-      <div className="ml-auto">
+export default class Modal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      description: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(key, e) {
+    this.setState({
+      [key]: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    const suggestion = {
+      title: this.state.title,
+      description: this.state.description
+    }
+    axios.post('http://localhost:5000/suggestion/', suggestion).then(res => {
+      navigate('/')
+    })
+  }
+
+  render() {
+    const { title, description } = this.state
+    return (
+      <div>
+        <button
+          type="button"
+          className="float-right text-center position-fixed fixed-bottom b-p"
+          data-toggle="modal"
+          data-target="#exampleModal"
+        >
+          <i className="fas fa-plus"></i>
+        </button>
+
         <div
           className="modal fade"
-          id="exampleModalCenter"
-          tabIndex="-1"
+          id="exampleModal"
+          // tabindex="-1"
           role="dialog"
-          aria-labelledby="exampleModalCenterTitle"
+          aria-labelledby="exampleModalLabel"
           aria-hidden="true"
         >
-          <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title pl-3" id="exampleModalLongTitle">
-                  Add Suggestion
-                </h5>
+                <h3>Add suggestion</h3>
                 <button
                   type="button"
                   className="close"
                   data-dismiss="modal"
                   aria-label="Close"
                 >
-                  <span aria-hidden="true">&times;</span>
+                  {/* <span aria-hidden="true">&times;</span> */}
                 </button>
               </div>
               <div className="modal-body">
-                <form className="col-lg-10">
-                  <div className="form-group">
-                    <label for="recipient-name" className="form-label l-modal">
-                      Title
-                    </label>
-                    <input
-                      type="text"
-                      className="border-danger form-control"
-                      id="recipient-name"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label for="message-text" className="form-label l-modal">
-                      Description
-                    </label>
-                    <textarea
-                      className="border-danger form-control"
-                      id="message-text"
-                    ></textarea>
-                  </div>
-                  <button type="button" className="btn btn-group-lg b-style">
-                    Add
-                  </button>
-                </form>
-              </div>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-dismiss="modal"
-                >
-                  Close
-                </button>
+                <div className="form-group">
+                  <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                      <label> Enter Title</label>
+                      <input
+                        onChange={elm => this.handleChange('title', elm)}
+                        value={title}
+                        className="form-control"
+                        type="text"
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>Description</label>
+                      <textarea
+                        onChange={elm => this.handleChange('description', elm)}
+                        value={description}
+                        className="form-control"
+                        id=""
+                        cols="50"
+                        rows="5"
+                        required
+                      ></textarea>
+                    </div>
+
+                    <button type="submit" className="btn d-flex float-right">
+                      Add Changes
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="c">
-        <button
-          className="btn fixed text-right"
-          display="static"
-          type="button"
-          data-toggle="modal"
-          data-target="#exampleModalCenter"
-        >
-          <i className="fab fa-medrt"></i>
-        </button>
-      </div>
-    </div>
-  )
+    )
+  }
 }
