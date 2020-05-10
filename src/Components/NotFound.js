a5efa0f01731854c7dab68ab3b5412dda32a24e0
wolvecode <wolvecode@gmail.com>
2020-05-10 14:13:26 +0100

    add image for page not founf

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import e2 from './IMG/e2.png'
 export default class NotFound extends React.Component {
   render() {
     return (
-      <div>
+      <div className="h-100">
         <h1 className="text-center pt-5 mt-5 text-danger">Not found</h1>
+        <img className="rounded mx-auto d-block" src={e2} alt="Not Found" />
       </div>
     )
   }
