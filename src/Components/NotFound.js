b901304c400f714ed863ee225586e7617f8aed7c
wolvecode <wolvecode@gmail.com>
2020-05-11 21:48:31 +0100

    fixed error page responsiveness

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -3,9 +3,13 @@ import e2 from './IMG/e2.png'
 export default class NotFound extends React.Component {
   render() {
     return (
-      <div className="h-100">
-        <h1 className="text-center pt-5 mt-5 text-danger">Not found</h1>
-        <img className="rounded mx-auto d-block" src={e2} alt="Not Found" />
+      <div className="">
+        <h1 className="text-center pt-5 text-danger">Page Not found</h1>
+        <img
+          className="col-10 rounded mx-auto d-block"
+          src={e2}
+          alt="Not Found"
+        />
       </div>
     )
   }
