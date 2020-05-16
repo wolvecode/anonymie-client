540959ef65229ee9658801d20e75839741021040
wolvecode <wolvecode@gmail.com>
2020-05-16 23:45:24 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import { Switch, Route, Redirect } from 'react-router-dom'
 import Login from './Components/Login'
 import CommentStat from './Components/CommentStat'
 import EditSuggestion from './Components/EditSuggestion'
@@ -11,12 +12,37 @@ import UserComment from './Components/UserComment'
 import NotFound from './Components/NotFound'
 import ListStared from './Components/ListStared'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    return (
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       auth: false
+//     }
+//   }
+//   render() {
+//     return (
+//       <AuthApi.Provider value={this.state.auth}>
+//         <Router>
+//           <Login path="/admin/login" />
+//           <UserComment path="/user/:id" />
+//           <SuggestionLog path="sug" />
+//           <General path="/" />
+//           <ListStared path="/stared/:id" />
+//           <NotFound path="*" />
+//           <ComThread path="/suggestion/comment/:id" />
+//           <EditSuggestion path="/edit/:id" />
+//           <CommentStat path="/stat" />
+//           <RouteRegistration path="/admin/reg" />
+//         </Router>
+//       </AuthApi.Provider>
+//     )
+//   }
+// }
+
+function App() {
+  const [auth, setAuth] = useState(false)
+  return (
+    <div>
       <Router>
         <Login path="/admin/login" />
         <UserComment path="/user/:id" />
@@ -29,8 +55,8 @@ class App extends React.Component {
         <CommentStat path="/stat" />
         <Register path="/admin/reg" />
       </Router>
-    )
-  }
+    </div>
+  )
 }
 
 export default App
