507f052d82aa1e8dcf01c97b3e6a747c8ec53b25
wolvecode <wolvecode@gmail.com>
2020-05-28 22:07:40 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,61 +1,67 @@
-import React, { useState } from 'react'
-// import { Switch, Route, Redirect } from 'react-router-dom'
+import React, { useState, Component } from 'react'
 import Login from './Components/Login'
 import CommentStat from './Components/CommentStat'
 import EditSuggestion from './Components/EditSuggestion'
 import SuggestionLog from './Components/SugThread'
-import { Router } from '@reach/router'
 import Register from './Components/Register'
 import ComThread from './Components/StaredComment'
 import General from './Components/General'
 import UserComment from './Components/UserComment'
 import NotFound from './Components/NotFound'
 import ListStared from './Components/ListStared'
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       auth: false
-//     }
-//   }
-//   render() {
-//     return (
-//       <AuthApi.Provider value={this.state.auth}>
-//         <Router>
-//           <Login path="/admin/login" />
-//           <UserComment path="/user/:id" />
-//           <SuggestionLog path="sug" />
-//           <General path="/" />
-//           <ListStared path="/stared/:id" />
-//           <NotFound path="*" />
-//           <ComThread path="/suggestion/comment/:id" />
-//           <EditSuggestion path="/edit/:id" />
-//           <CommentStat path="/stat" />
-//           <RouteRegistration path="/admin/reg" />
-//         </Router>
-//       </AuthApi.Provider>
-//     )
-//   }
-// }
+import AuthApi from './utils/AuthApi'
+import {
+  BrowserRouter as Routers,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
 
 function App() {
   const [auth, setAuth] = useState(false)
   return (
     <div>
-      <Router>
-        <Login path="/admin/login" />
-        <UserComment path="/user/:id" />
-        <SuggestionLog path="sug" />
-        <General path="/" />
-        <ListStared path="/stared/:id" />
-        <NotFound path="*" />
-        <ComThread path="/suggestion/comment/:id" />
-        <EditSuggestion path="/edit/:id" />
-        <CommentStat path="/stat" />
-        <Register path="/admin/reg" />
-      </Router>
+      <AuthApi.Provider value={{ auth, setAuth }}>
+        <Routers>
+          <Switch>
+            <Route path="/admin/login" component={Login} />
+            <Route path="/user/:id" component={UserComment} />
+            <Route exact path="/" component={General} />
+            <RouteProtected path="/sug" component={SuggestionLog} auth={auth} />
+            <Route path="/stared/:id" component={ListStared} />
+            <Route path="/suggestion/comment/:id" component={ComThread} />
+            <Route path="/edit/:id" component={EditSuggestion} />
+            <Route path="/stat" component={CommentStat} />
+            <Route path="/admin/reg" component={Register} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Routers>
+      </AuthApi.Provider>
     </div>
+  )
+}
+
+const RouteProtected = ({ component: Component, ...rest }) => {
+  const authApi = React.useContext(AuthApi)
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authApi.auth ? <Component {...props} /> : <Redirect to="/admin/login" />
+      }
+    />
+  )
+}
+
+const RouteGeneral = ({ component: Component, ...rest }) => {
+  const authApi = React.useContext(AuthApi)
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authApi.auth ? <Component {...props} /> : <Redirect to="/sug" />
+      }
+    />
   )
 }
 
