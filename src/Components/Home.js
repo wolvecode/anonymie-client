ccff48fbc864b3c97912d8a7a5a77544c2c2c1fb
wolvecode <wolvecode@gmail.com>
2020-06-19 08:32:35 +0100

    add loader to handle when suggestion is yet to be available

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Nav from './Nav'
 import Moment from 'react-moment'
 import 'moment-timezone'
+import Loader from 'react-loader-spinner'
 import axios from 'axios'
 import '../style/general.css'
 
@@ -10,7 +11,8 @@ export default class General extends React.Component {
     super(props)
     this.state = {
       thread: [],
-      searchTerms: ''
+      searchTerms: '',
+      suggestionSize: ''
     }
     this.updateSearch = this.updateSearch.bind(this)
   }
@@ -19,7 +21,8 @@ export default class General extends React.Component {
       .post(process.env.baseUrl + '/suggestion/')
       .then(res => {
         this.setState({
-          thread: res.data.suggestion
+          thread: res.data.suggestion,
+          suggestionSize: res.data.sugggestionSize
         })
       })
       .catch(err => {
@@ -57,39 +60,58 @@ export default class General extends React.Component {
 
         <div>
           <div className="col-md-6 offset-md-3 col-lg-6 offset-lg-3 ab">
-            {this.state.thread.map(data => (
-              <div key={data._id} id="cont" className="my-2">
-                <div id="comments">
-                  <div className="cmmnt px-4">
-                    <div className="cmmnt-content">
-                      <header>
-                        <a href={'/user/' + data._id} className="userlink">
-                          {data.title}
-                        </a>
-                        &nbsp; - &nbsp;
-                        <span className="pubdate">
-                          <Moment toNow>{data.date}</Moment>
-                        </span>
-                      </header>
-                      <div className="descripton">
-                        <a href={'/user/' + data._id}>
-                          <p>
-                            {data.description
-                              .split(' ')
-                              .splice(0, 50)
-                              .join(' ')}
-                          </p>
-                        </a>
+            {this.state.suggestionSize.length === 0 ? (
+              <div
+                style={{
+                  display: 'flex',
+                  height: '30rem',
+                  justifyContent: 'center',
+                  alignItems: 'center'
+                }}
+              >
+                <Loader
+                  type="Circles"
+                  color="grey"
+                  height={100}
+                  width={100}
+                  // timeout={5000} //3 secs
+                />
+              </div>
+            ) : (
+              this.state.thread.map(data => (
+                <div key={data._id} id="cont" className="my-2">
+                  <div id="comments">
+                    <div className="cmmnt px-4">
+                      <div className="cmmnt-content">
+                        <header>
+                          <a href={'/user/' + data._id} className="userlink">
+                            {data.title}
+                          </a>
+                          &nbsp; - &nbsp;
+                          <span className="pubdate">
+                            <Moment toNow>{data.date}</Moment>
+                          </span>
+                        </header>
+                        <div className="descripton">
+                          <a href={'/user/' + data._id}>
+                            <p>
+                              {data.description
+                                .split(' ')
+                                .splice(0, 50)
+                                .join(' ')}
+                            </p>
+                          </a>
+                        </div>
+                        {/* Number Of Comment In A Certain Suggestion */}
+                        {/* <div className="count text-primary">
+                            <i className="far fa-comment"></i> &nbsp;
+                          </div> */}
                       </div>
-                      {/* Number Of Comment In A Certain Suggestion */}
-                      {/* <div className="count text-primary">
-                        <i className="far fa-comment"></i> &nbsp;
-                      </div> */}
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
         </div>
       </div>
